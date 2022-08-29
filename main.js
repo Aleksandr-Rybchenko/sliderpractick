5cdeb8ca4f4d86f79c8467daadf9caa1c78d4402
blabin777@gmail.com <blabin777@gmail.com>
2022-08-29 23:23:27 +0300

    first commit

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-const text2 = document.querySelector('.text2')
-const text1 = document.querySelector('.text1')
-const text3 = document.querySelector('.text3')
-const btn2 = document.querySelector('.btn2')
-const btn1 = document.querySelector('.btn1')
-const btn3 = document.querySelector('.btn3')
+const texts = document.querySelectorAll('.text')
+const btns = document.querySelectorAll('.btn')
+
+const changeTabHandler = (idx) => {
 
 
-btn1.addEventListener('click', () => {
-  if ((text2.style.display = 'block') || (text3.style.display = 'block')) {
-    text2.style.display = 'none'
-    text3.style.display = 'none'
-    text1.style.display = 'block'
-  }
-})
-btn2.addEventListener('click', () => {
-  if ((text1.style.display = 'block') || (text3.style.display = 'block')) {
-    text1.style.display = 'none'
-    text3.style.display = 'none'
-    text2.style.display = 'block'
-  }
-})
-btn3.addEventListener('click', () => {
-  if ((text1.style.display = 'block') || (text2.style.display = 'block')) {
-    text1.style.display = 'none'
-    text2.style.display = 'none'
-    text3.style.display = 'block'
-  }
-})
+  console.log(idx)
+  texts.forEach((elem, index) => {
+    if (idx === index) {
+      elem.style.display = 'block'
+
+
+    } else {
+
+      elem.style.display = 'none'
+    }
+  })
+}
+
+const initTabs = () => {
+  texts.forEach((elem) => {
+    elem.style.display = 'none'
+  })
+  btns.forEach((el, ind) => {
+    console.log(ind);
+    el.addEventListener('click', () => changeTabHandler(ind))
+  })
+}
+const start = () => {
+  initTabs()
+}
+start()
